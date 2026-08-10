4f05e5d5c460ca6378a6916319b2adb1464eb0dc
Triefth <da.cifuentesh@duocuc.cl>
2026-08-10 19:23:33 -0400

    prime update
    👍

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,29 +4,31 @@ resource "aws_apigatewayv2_api" "mi_api_gateway" {
   protocol_type = "HTTP"
 }
 
-# 2. Creación de la Integración (Apuntando a la API de mindicador.cl)
+# 2. Creación de la Integración (Apuntando a mindicador.cl)
 resource "aws_apigatewayv2_integration" "integracion_mindicador" {
-  api_id             = aws_apigatewayv2_api.mi_api_gateway.id
-  integration_type   = "HTTP_PROXY"
-  integration_uri    = "https://mindicador.cl/api"
-  integration_method = "GET"
+  api_id                 = aws_apigatewayv2_api.mi_api_gateway.id
+  integration_type       = "HTTP_PROXY"
+  integration_uri        = "https://mindicador.cl/api"
+  integration_method     = "GET"
+  payload_format_version = "1.0"
 }
 
-# 3. Creación de la Ruta asociada al método GET y a la integración
+# 3. Creación de la Ruta
 resource "aws_apigatewayv2_route" "ruta_datos" {
   api_id    = aws_apigatewayv2_api.mi_api_gateway.id
   route_key = "GET /datos"
   target    = "integrations/${aws_apigatewayv2_integration.integracion_mindicador.id}"
 }
 
-# 4. Creación del Stage y despliegue automático
+# 4. Creación del Stage
 resource "aws_apigatewayv2_stage" "stage_desarrollo" {
   api_id      = aws_apigatewayv2_api.mi_api_gateway.id
   name        = "Desarrollo"
   auto_deploy = true
 }
 
-# (Opcional) Mostrar la URL final de invocación en la consola al terminar
+# Output con la URL completa e incluye el Stage "Desarrollo"
 output "api_endpoint" {
-  value = "${aws_apigatewayv2_api.mi_api_gateway.api_endpoint}/datos"
+  value       = "${aws_apigatewayv2_api.mi_api_gateway.api_endpoint}/${aws_apigatewayv2_stage.stage_desarrollo.name}/datos"
+  description = "URL final para probar en Postman o Curl"
 }
